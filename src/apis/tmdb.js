0794dccc5f316b03ccc4a43ab5a59e192f655db3
dodisutarya <ikhwands@protonmail.ch>
2022-07-21 21:33:22 +0700

    configure key to deploy

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '01e3741f93244a45e88c3597f2456e9d';
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const baseUrl = 'https://api.themoviedb.org/3/';
 
 const tmdb = axios.create({
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,15 +4,15 @@ import { getAuth } from 'firebase/auth';
 // TODO: replace with your own config
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBe_wv_-KEGwp5kzmvnU4NiONTpnKesVFQ",
-    authDomain: "pair-55-dts-mini-project.firebaseapp.com",
-    projectId: "pair-55-dts-mini-project",
-    storageBucket: "pair-55-dts-mini-project.appspot.com",
-    messagingSenderId: "1011447497449",
-    appId: "1:1011447497449:web:2866ecb5f479bc22eda678",
-    measurementId: "G-V6RWV61HK5"
-  };
-  
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "dos-apps-auth.firebaseapp.com",
+  projectId: "dos-apps-auth",
+  storageBucket: "dos-apps-auth.appspot.com",
+  messagingSenderId: "145779135092",
+  appId: "1:145779135092:web:901d53e7a16829de6df721"
+};
+
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
